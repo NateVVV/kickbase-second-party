5053de4166725e4b078bab9450cb2bac90ef91b1
Nathanael Wenzel <nathanael.wenzel97@gmail.com>
2021-05-08 09:59:21 +0200

    adding Login component

---
+++ src/components/Login.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-container>
+        <v-text-field label="Email" v-model="email"></v-text-field>
+        <v-text-field
+            label="Password"
+            type="password"
+            v-model="password"
+        ></v-text-field>
+        <v-btn @click="login">Login</v-btn>
+    </v-container>
+</template>
+
+<script>
+import { login } from "@/lib/kickbase.js";
+export default {
+    name: "login",
+    props: {
+        email: String,
+        password: String,
+    },
+    methods: {
+        login: async function() {
+            let { user, token, leagues } = await login(
+                this.email,
+                this.password
+            );
+            this.$emit("loggedIn", { user, token, leagues });
+        },
+    },
+};
+</script>
