bb18890ffc9ded78e530332ab37db07ec37f9626
Nathanael Wenzel <nathanael.wenzel97@gmail.com>
2021-05-08 09:25:14 +0200

    adding login and leagueInfo functions for kickbase

---
+++ src/lib/kickbase.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+async function login(email, password) {
+    try {
+        let { data, status } = await axios.post(
+            "https://europe-west3-kickbase-312916.cloudfunctions.net/login",
+            {
+                email: email,
+                password: password,
+                ext: false,
+            }
+        );
+        console.log(`status Code: ${status}`);
+        let { token, leagues } = data;
+        return { token, leagues };
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function leagueInfo(leagueId, accessToken) {
+    try {
+        let { status, data } = await axios.post(
+            "https://europe-west3-kickbase-312916.cloudfunctions.net/leagueInfo",
+            {
+                token: accessToken,
+                leagueId: leagueId,
+            }
+        );
+        console.log(`status code: ${status}`);
+        return data;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+export { login, leagueInfo };
