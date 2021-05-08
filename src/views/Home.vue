dad0efdcb40ff1eff32d90fe194fd6a08a49016f
Nathanael Wenzel <nathanael.wenzel97@gmail.com>
2021-05-08 09:27:13 +0200

    using basic login and leagueInfo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,41 @@
 <template>
-  <hello-world />
+    <v-container>
+        <v-text-field label="Email" v-model="email"></v-text-field>
+        <v-text-field
+            label="Password"
+            type="password"
+            v-model="password"
+        ></v-text-field>
+        <v-btn @click="login">Login</v-btn>
+    </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+import * as kickbase from "@/lib/kickbase.js";
+import credentials from "@/lib/credentials.js";
 
-  export default {
-    name: 'Home',
+export default {
+    name: "Home",
 
-    components: {
-      HelloWorld,
+    components: {},
+
+    data: () => ({
+        email: credentials.email,
+        password: credentials.password,
+    }),
+
+    methods: {
+        login: async function() {
+            let { token, leagues } = await kickbase.login(
+                this.email,
+                this.password
+            );
+            console.log(token);
+            console.log(leagues);
+            let binBlau = leagues[0];
+            let leagueInfo = await kickbase.leagueInfo(binBlau.id, token);
+            console.log(leagueInfo);
+        },
     },
-  }
+};
 </script>
