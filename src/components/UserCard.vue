bb0c9cbe26032b2ef88df9ee5a32d295804a6a10
Nathanael Wenzel <nathanael.wenzel97@gmail.com>
2021-05-08 13:06:53 +0200

    set image size in UserCard

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,7 +1,7 @@
 <template>
     <v-container>
-        <v-card>
-            <v-img height="150" :src="user.profile"></v-img>
+        <v-card class="mx-auto" max-width="344">
+            <v-img height="344px" :src="user.profile"></v-img>
             <v-card-title>{{ user.name }}</v-card-title>
             <v-card-text>{{ user.email }}</v-card-text>
         </v-card>
