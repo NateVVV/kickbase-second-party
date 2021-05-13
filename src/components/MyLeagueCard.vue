389838f344b9f07ed982ea20b87c0dab7b663116
Nathanael Wenzel <nathanael.wenzel97@gmail.com>
2021-05-13 13:14:50 +0200

    added MyLeagueCard for a quick overview

---
+++ src/components/MyLeagueCard.vue
@@ -0,0 +1,79 @@
+<template>
+    <v-card class="mx-auto" max-width="344">
+        <div class="d-flex justify-space-between caption ma-2">
+            <div>
+                <div class="grey--text mb-2">
+                    Budget
+                </div>
+
+                <div
+                    class="font-weight-medium"
+                    :class="{ 'green--text': league.budget > 0 }"
+                >
+                    {{ budget }} €
+                </div>
+            </div>
+
+            <div>
+                <div class="grey--text mb-2">
+                    Teamvalue
+                </div>
+
+                <div class="font-weight-medium">{{ teamValue }} €</div>
+            </div>
+
+            <div>
+                <div class="grey--text mb-2">
+                    Placement
+                </div>
+
+                <div class="font-weight-medium">{{ league.placement }}.</div>
+            </div>
+
+            <div>
+                <div class="grey--text text-darken-1 mb-2">
+                    Points
+                </div>
+
+                <div class="font-weight-medium">
+                    {{ points }}
+                </div>
+            </div>
+        </div>
+    </v-card>
+</template>
+
+<script>
+import MyLeague from "@/lib/models/my_league.js";
+
+export default {
+    name: "MyLeagueCard",
+    props: {
+        league: MyLeague,
+    },
+    data: () => ({}),
+    computed: {
+        teamValue: function() {
+            return this.league.teamValue
+                ? this.league.teamValue
+                      .toString()
+                      .replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+                : 0;
+        },
+        budget: function() {
+            return this.league.budget
+                ? this.league.budget
+                      .toString()
+                      .replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+                : 0;
+        },
+        points: function() {
+            return this.league.points
+                ? this.league.points
+                      .toString()
+                      .replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+                : 0;
+        },
+    },
+};
+</script>
--- src/views/League.vue
+++ src/views/League.vue
@@ -1,12 +1,14 @@
 <template>
     <v-container>
         <LeagueDataCard :league="league"></LeagueDataCard>
+        <MyLeagueCard :league="myLeague" v-if="myLeague != null"></MyLeagueCard>
     </v-container>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 import LeagueDataCard from "@/components/LeagueDataCard.vue";
+import MyLeagueCard from "@/components/MyLeagueCard.vue";
 import LeagueData from "@/lib/models/league_data.js";
 import { myLeagueInfo } from "@/lib/kickbase.js";
 
@@ -15,14 +17,22 @@ export default {
     props: {
         league: LeagueData,
     },
-    components: { LeagueDataCard },
-    data: () => ({}),
+    components: { LeagueDataCard, MyLeagueCard },
+    data: () => ({
+        myLeague: null,
+    }),
     computed: {
         ...mapGetters(["token"]),
     },
     created: async function() {
         let leagueInfo = await myLeagueInfo(this.league.id, this.token);
         console.log(leagueInfo);
+        this.myLeague = leagueInfo;
+    },
+    watch: {
+        async league() {
+            this.myLeague = await myLeagueInfo(this.league.id, this.token);
+        },
     },
 };
 </script>
